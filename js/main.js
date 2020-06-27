02e536f371562a02ac21d6e647f280953f6fee39
Gergo <gergor6]gmail.com>
2020-06-27 13:57:32 +0200

    rendeles js updated

--- js/main.js
+++ js/main.js
@@ -3,3 +3,18 @@ function calcAmount() {
     let amountInput = document.querySelector("input[name='amount-input']");
 
 }
+
+
+function singleSelectChangeValue() {
+    //Getting Value
+    
+    // METHOD 1
+    var selValue = document.getElementById("hamburgerValasztas").value;
+    
+    //METHOD 2
+    var selObj = document.getElementById("hamburgerValasztas");
+    var selValue = selObj.options[selObj.selectedIndex].value;
+    
+    //Setting Value
+    document.getElementById("hamburgerValasztasJS").value = selValue;
+}
