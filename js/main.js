517b2f1bdc5e844a0ce6ff2097b4d859e504e729
GergoR6 <gergor6@gmail.com>
2020-07-06 21:07:05 +0200

    rendelés function

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 function calcAmount() {
-    
+
     let amountInput = document.querySelector("input[name='amount-input']");
 
 }
@@ -7,14 +7,38 @@ function calcAmount() {
 
 function singleSelectChangeValue() {
     //Getting Value
-    
     // METHOD 1
     var selValue = document.getElementById("hamburgerValasztas").value;
-    
     //METHOD 2
     var selObj = document.getElementById("hamburgerValasztas");
     var selValue = selObj.options[selObj.selectedIndex].value;
-    
     //Setting Value
     document.getElementById("hamburgerValasztasJS").value = selValue;
 }
+
+
+function singleSelectChangeValue2() {
+    //Getting Value
+    // METHOD 1
+    var selValue = document.getElementById("pizzaValasztas").value;
+    //METHOD 2
+    var selObjPizza = document.getElementById("pizzaValasztas");
+    var selValue = selObjPizza.options[selObjPizza.selectedIndex].value;
+    //Setting Value
+    document.getElementById("pizzaValasztasJS").value = selValue;
+}
+
+
+function singleSelectChangeValue3() {
+    //Getting Value
+    // METHOD 1
+    var selValue = document.getElementById("uditoValasztas").value;
+    //METHOD 2
+    var selObjUdito = document.getElementById("uditoValasztas");
+    var selValue = selObjUdito.options[selObjUdito.selectedIndex].value;
+    //Setting Value
+    document.getElementById("uditoValasztasJS").value = selValue;
+}
+
+
+
